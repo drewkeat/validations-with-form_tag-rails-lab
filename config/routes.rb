78f5958f83393c9c5a2a56ebcc25afa30dacadf7
Drew Keat <thedrewkeat@gmail.com>
2021-07-23 12:34:10 -0600

    posts errors done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :posts, only: [:edit, :update, :show]
+
+  resources :authors, only: [:new, :create, :show]
+
 end
